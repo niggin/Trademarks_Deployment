70fa7847124f852957bd115fefe0c4b80b38ba78
Niggin <rubik303@mail.ru>
2014-03-24 19:18:46 +0400

    MySQL support

--- trademarks/models.py
+++ trademarks/models.py
@@ -4,5 +4,8 @@ class Word(models.Model):
     word = models.CharField(max_length=50)
     ipa = models.CharField(max_length=50)
     meaning = models.CharField(max_length=1000)
+    lang = models.CharField(max_length=5)
     def __str__(self):
+        return self.word
+    def __unicode__(self):
         return self.word

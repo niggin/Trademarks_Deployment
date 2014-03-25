6e3f94afe0d7b4307c835bb5928edeea59140104
Niggin <rubik303@mail.ru>
2014-03-25 23:28:02 +0400

    few fixes with database
    fun metrics also has been included

--- trademarks/templatetags/search_extra.py
+++ trademarks/templatetags/search_extra.py
@@ -1,7 +1,13 @@
 from django import template
+import random
 
 register = template.Library()
 
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+@register.simple_tag
+def random_quote():
+    quote = random.randint(0,100)
+    return quote

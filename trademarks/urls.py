5e36d4c3c6308a5f68cf5e79eecc61d7d46f5a20
Niggin <rubik303@mail.ru>
2014-03-29 00:25:14 +0400

    Partly implemented sorting by match

--- trademarks/urls.py
+++ trademarks/urls.py
@@ -8,4 +8,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.home, name='home'),
     url(r'^search$', views.search, name='search'),
+    url(r'^search_sortbymatch$', views.search_sortbymatch, name='search_sortbymatch'),
 )
--- trademarks/views.py
+++ trademarks/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 from trademarks.models import Word
 from trademarks.algorithm import *
+import random
 
 def home(request):
     context = {'forsearch': ''}
@@ -10,6 +11,10 @@ def home(request):
 
 def search(request):
     input = request.GET['findme']
+    try:
+        sorting = request.GET['matchsort']
+    except:
+        sorting = '0'
     p = DoubleMetaphon(input).transcription
     data = dict()
     final = dict()
@@ -23,12 +28,19 @@ def search(request):
                 for i in [0,1,2]:
                     data[item.lang].append(list())
             if item.ipa == p:
-                data[item.lang][0].append(item)
+                data[item.lang][0].append([item, random.randint(0,100)])
             elif item.ipa.startswith(p):
-                data[item.lang][1].append(item)
+                data[item.lang][1].append([item, random.randint(0,100)])
             else:
-                data[item.lang][2].append(item)
+                data[item.lang][2].append([item, random.randint(0,100)])
         for lang in langs:
+            if sorting != '0':
+                for i in [0,1,2]:
+                    data[lang][i].sort(key=lambda l:int(l[1]), reverse = True)
             final[lang] = data[lang][0] + data[lang][1] + data[lang][2]
-    context = {'array' : final, 'forsearch': input, 'langs' : langs, 'debug' : p}
+    context = {'array' : final, 'forsearch': input, 'langs' : langs, 'debug' : p + sorting, 'sort' : sorting}
     return render(request, 'search.html', context)
+
+def search_sortbymatch(request, array):
+
+    return render(request, 'index.html', {})
